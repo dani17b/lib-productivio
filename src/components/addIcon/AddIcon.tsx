638598554befd26a9613b31107533c3480b3095e
Marcos Martínez <marcos.martinez.fernandez@altia.es>
2023-03-16 09:34:10 +0100

    Import addIcon component and documentated on storybook

---
+++ src/components/addIcon/AddIcon.tsx
@@ -0,0 +1,25 @@
+import { IoMdPersonAdd } from 'react-icons/io';
+import { IconContext } from 'react-icons';
+import 'src/components/addIcon/addIcon.scss';
+import { MouseEventHandler } from 'react';
+
+export interface IconProps {
+  /**
+   * Function associated to the button
+   */
+  action?: MouseEventHandler;
+}
+
+export const AddIcon = (props: IconProps) => {
+  const { action } = props;
+
+  return (
+    <IconContext.Provider
+      value={{ color: 'white', size: '20px', className: 'rounded-button' }}
+    >
+      <div>
+        <IoMdPersonAdd onClick={action} />
+      </div>
+    </IconContext.Provider>
+  );
+};
