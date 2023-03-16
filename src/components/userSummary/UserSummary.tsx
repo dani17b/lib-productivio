f5f91ae44693eef9ab0e6f8616430b4578502964
Paula Seoane <paula.seoane@altia.es>
2023-03-16 17:25:30 +0100

    #4 UserSummary's props changed (documentation not updated yet)

--- src/components/userSummary/UserSummary.tsx
+++ src/components/userSummary/UserSummary.tsx
@@ -1,33 +1,22 @@
 import { useState, useEffect } from 'react';
-import { ContactsNumberProps, CurrentTasksProps, LevelProps } from './actions';
 import './userSummary.scss';
 
-export const UserSummary = (props: any) => {
-  const [level, setLevel] = useState<LevelProps>({
+interface UserSummaryProps {
+  username: string;
+  totalPoints: number;
+  level: number;
+  contactsNumber: number;
+  currentTasks: number;
+}
+
+export const UserSummary = (props: UserSummaryProps) => {
+  const [userProps, setUserProps] = useState<UserSummaryProps>({
     //TODO Añadir lógica real
     username: 'Logica pendiente',
     totalPoints: 2,
     level: 1,
-  });
-
-  const updateLevel = (points: number) => {
-    const pointsPerLevel = 350;
-    const currentLevel = Math.floor(points / pointsPerLevel) + 1;
-    setLevel((previousLevel) => ({ ...previousLevel, level: currentLevel }));
-  };
-
-  useEffect(() => {
-    updateLevel(level.totalPoints);
-  }, [level.totalPoints]);
-
-  const [contacts /*setContacts*/] = useState<ContactsNumberProps>({
-    //TODO Añadir lógica real
-    contactsNumber: 4,
-  });
-
-  const [tasks /*setTasks*/] = useState<CurrentTasksProps>({
-    //TODO Añadir lógica real
-    currentTasks: 2,
+    contactsNumber: 123,
+    currentTasks: 3,
   });
 
   return (
@@ -37,19 +26,20 @@ export const UserSummary = (props: any) => {
       </div>
       <div className="user-summary__center-column">
         <div className="user-summary__username">
-          danituriño22
-          {/* {userInfo.login} */}
+          danituriño22 {userProps.username}
         </div>
         <div className="user-summary__current-missions">
-          {tasks.currentTasks} misiones en curso
+          {userProps.currentTasks} misiones en curso
         </div>
-        <div className="user-summary__user-level">Nivel {level.level}</div>
+        <div className="user-summary__user-level">Nivel {userProps.level}</div>
       </div>
       <div className="user-summary__right-column">
         <div className="user-summary__contacts">
-          {contacts.contactsNumber} contactos
+          {userProps.contactsNumber} contactos
         </div>
-        <div className="user-summary__total-points">{level.totalPoints}pts</div>
+        <div className="user-summary__total-points">
+          {userProps.totalPoints}pts
+        </div>
       </div>
     </div>
   );
