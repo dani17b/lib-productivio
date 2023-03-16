a41428e4039f61a5cccacc9dbb0189702e7942ab
ALTIA\daniel.turino <daniel.turino@altia.es>
2023-03-16 14:02:30 +0100

    Added rollup config to generate dists

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+const resolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const dts = require("rollup-plugin-dts").default;
+const scss = require("rollup-plugin-scss");
+
+const packageJson = require("./package.json");
+
+module.exports = [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: false,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: false,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      scss()
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    external: [/\.scss$/],
+    plugins: [dts()],
+  },
+];
