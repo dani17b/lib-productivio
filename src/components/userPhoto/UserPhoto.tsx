558835a773b5f1c80a0a312b6bca91b1d9eeca0f
Ivan Lopez Rodriguez <ivan.lopez.rodriguez@altia.es>
2023-03-16 08:53:24 +0100

    stories mdx added and eslint in UserPhoto.tsx

--- src/components/userPhoto/UserPhoto.tsx
+++ src/components/userPhoto/UserPhoto.tsx
@@ -1,25 +1,26 @@
-import React from "react";
-import "./userPhoto.scss";
+import React from 'react';
+import './userPhoto.scss';
 
 interface UserPhotoProps {
   imageSrc: string;
   borderColor: string;
 }
 
-export const UserImage: React.FC<UserPhotoProps> = ({ imageSrc, borderColor }) => {
+export const UserImage: React.FC<UserPhotoProps> = ({
+  imageSrc,
+  borderColor,
+}) => {
   const circleStyle = {
     borderColor: borderColor,
   };
 
   return (
     <div className="square">
-    <div className="circle-image-container">
-      <div className="circle" style={circleStyle}>
-        <img src={imageSrc} alt="User" className="circle-image" />
+      <div className="circle-image-container">
+        <div className="circle" style={circleStyle}>
+          <img src={imageSrc} alt="User" className="circle-image" />
+        </div>
       </div>
-    </div>
     </div>
   );
 };
-
-
