328b3e7bd6106ed020920e24a8e547b0bd7e620d
Paula Seoane <paula.seoane@altia.es>
2023-03-16 10:12:58 +0100

    #4 UserSummary documentation quick fix (actions.ts re-added)

--- src/components/userSummary/UserSummary.tsx
+++ src/components/userSummary/UserSummary.tsx
@@ -1,35 +1,6 @@
 import { useState, useEffect } from 'react';
-// import { ContactsNumberProps, CurrentTasksProps, LevelProps } from './actions';
+import { ContactsNumberProps, CurrentTasksProps, LevelProps } from './actions';
 import './userSummary.scss';
-
-interface LevelProps {
-  /**
-   * Label that contains the user's username
-   */
-  username: string;
-  /**
-   * User's total points
-   */
-  totalPoints: number;
-  /**
-   * User's current level
-   */
-  level: number;
-}
-
-interface ContactsNumberProps {
-  /**
-   * User's total contacts
-   */
-  contactsNumber: number;
-}
-
-interface CurrentTasksProps {
-  /**
-   * Total number of current missions/tasks
-   */
-  currentTasks: number;
-}
 
 export const UserSummary = (props: any) => {
   const [level, setLevel] = useState<LevelProps>({
