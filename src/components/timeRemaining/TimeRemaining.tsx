85e978f0e18a3093f67346a5d2f45e34882fc911
Bruno López <bruno.lpgz@gmail.com>
2023-03-20 16:40:53 +0100

    Mejorado

--- src/components/timeRemaining/TimeRemaining.tsx
+++ src/components/timeRemaining/TimeRemaining.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+// import {useState} from 'react';
 import './timeRemaining.scss';
 
 export interface TimeRemainingProps {
-    /**
+  /**
    * Date of completion
    */
   dueTime: Date;
 }
 
+// const [timeLeft, setTimeLeft] = useState(0);
+
 export const TimeRemaining = (props: TimeRemainingProps) => {
-  return <div>Quedan {(new Date()).toLocaleString('es-ES')}</div>;
+  const msRemaining = props.dueTime.getTime() - new Date().getTime();
+  const daysRemaining = Math.floor(msRemaining / (1000 * 60 * 60 * 24));
+  const hoursRemaining = Math.floor(
+    (msRemaining - daysRemaining * (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  return (
+    <div>
+      Quedan {daysRemaining} días y {hoursRemaining} horas.
+    </div>
+  );
+};
+
+TimeRemaining.defaultProps = {
+  dueTime: new Date('2023-06-27'),
 };
