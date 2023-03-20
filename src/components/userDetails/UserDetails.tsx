dbee4cbdc81a40831247840901b4aaf0457d6713
Xavier <xnovoavarela@gmail.com>
2023-03-20 16:45:13 +0100

    Changes needed to make teamDropdown work to be exported

--- src/components/userDetails/UserDetails.tsx
+++ src/components/userDetails/UserDetails.tsx
@@ -1,33 +1,31 @@
 import './userDetails.scss';
 
 export interface UserDetailsProps {
-  user: {
     username: string;
     description: string;
     score: number; // TODO: if stars are deleted, delete this
     profileColor: string;
     img: string;
-  };
 }
 
-export const UserDetails = (props: UserDetailsProps) => {
-  const { user } = props;
+export const UserDetails = ({ user }: { user: UserDetailsProps }) => {
+  const { username, description, img } = user;
 
   return (
     <div className="user-details">
       {/* TODO implementar el componente de la foto de prefil */}
       <div className="user-details__pic">
-        <img src={user.img} alt="User Profile" />
+        <img src={img} alt="User Profile" />
       </div>
       <div className="user-details__info">
         <div className="user-details__info__top">
           <div className="user-details__info__top__username-star">
-            <p>{user.username}</p>
+            <p>{username}</p>
             {/* TODO: implementar componente estrellas o borrarlo (como en el prototipo)*/}
             <p>estrellas</p>
           </div>
           <div className="user-details__info__bottom">
-            <p>{user.description}</p>
+            <p>{description}</p>
           </div>
         </div>
       </div>
