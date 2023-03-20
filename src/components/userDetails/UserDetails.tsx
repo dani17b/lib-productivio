da6887115dfba1a5a0fd1486477d998a1393e75d
hector.arias <ariashector19@gmail.com>
2023-03-20 15:57:18 +0100

    Refactoring UserDetail code for implementacion

--- src/components/userDetails/UserDetails.tsx
+++ src/components/userDetails/UserDetails.tsx
@@ -2,31 +2,27 @@ import './userDetails.scss';
 import { UserPhoto } from '../userPhoto/UserPhoto';
 
 export interface UserDetailsProps {
-  user: {
-    username: string;
-    description: string;
-    profileColor: string;
-    img: string;
-  };
+  username: string;
+  description: string;
+  profileColor: string;
+  img: string;
 }
 
-export const UserDetails = (props: UserDetailsProps) => {
-  const { user } = props;
+export const UserDetails = ({ user }: { user: UserDetailsProps }) => {
+  const { username, description, profileColor, img } = user;
 
   return (
     <div className="user-details">
       <div className="user-details__pic">
-        <UserPhoto imageSrc={user.img} borderColor={user.profileColor} />
+        <UserPhoto imageSrc={img} borderColor={profileColor} />
       </div>
       <div className="user-details__info">
         <div className="user-details__info__top">
-          <div className="user-details__info__top__username-star">
-            {user.username}
-          </div>
+          <div className="user-details__info__top__username">{username}</div>
         </div>
         <div className="user-details__info__bottom">
           <div className="user-details__info__bottom_description">
-            <p>{user.description}</p>
+            <p>{description}</p>
           </div>
         </div>
       </div>
