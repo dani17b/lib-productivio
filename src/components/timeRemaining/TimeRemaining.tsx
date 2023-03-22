81868b0a3ad253082eb6aeefdb94ebce082c60e9
Bruno López <bruno.lpgz@gmail.com>
2023-03-22 10:22:07 +0100

    wip

--- src/components/timeRemaining/TimeRemaining.tsx
+++ src/components/timeRemaining/TimeRemaining.tsx
@@ -7,28 +7,69 @@ export interface TimeRemainingProps {
    * Date of completion
    */
   dueTime: Date;
+
+  /**
+   * Display with only one time unit? (8h instead of 8h 20min)
+   */
+  simpleDisplay?: boolean;
 }
 
-export const TimeRemaining = (props: TimeRemainingProps) => {
+const getMsRemaining = (dueTime: Date) => {};
+
+const getTimeRemaining = (dueTime: Date) => {
   const msOnADay = 1000 * 60 * 60 * 24;
   const msOnAnHour = 1000 * 60 * 60;
-  
-  const msRemaining = props.dueTime.getTime() - new Date().getTime();
-  const daysRemaining = Math.floor(msRemaining / msOnADay);
-  const hoursRemaining = Math.floor(
-    (msRemaining - daysRemaining * msOnADay) / msOnAnHour
+  const msOnAnMinute = 1000 * 60;
+
+  const totalMilliseconds = dueTime.getTime() - new Date().getTime();
+  const days: number = +Math.floor(totalMilliseconds / msOnADay);
+  const hours: number = +Math.floor(
+    (totalMilliseconds - days * msOnADay) / msOnAnHour
+  );
+  const minutes: number = +Math.floor(
+    totalMilliseconds - (days * msOnADay + hours * msOnAnHour) / msOnAnMinute
   );
 
+  return [
+    [days, 'd'],
+    [hours, 'h'],
+    [minutes, 'min'],
+  ];
+};
+
+const getTimeText = (props: TimeRemainingProps) => {
+  const totalMilliseconds = props.dueTime.getTime() - new Date().getTime();
+  let result;
+
+  if (totalMilliseconds > 0) {
+    result = '';
+    for (const [value, unitType] of getTimeRemaining(props.dueTime)) {
+      if (value > 0) {
+        result += `${value}${unitType} `;
+        if (props.simpleDisplay) {
+          break;
+        }
+      }
+    }
+
+    if(result =''){
+      
+    }
+  } else {
+    result = (
+      <span style={{ color: 'red', fontWeight: 'bold', fontSize: '0.75em' }}>
+        EXPIRADO
+      </span>
+    );
+  }
+  return result;
+};
+
+export const TimeRemaining = (props: TimeRemainingProps) => {
   return (
     <div className="time-remaining">
       <TbClockFilled className="time-remaining__icon" size={24}></TbClockFilled>
-      {msRemaining > 0 ? (
-        `${daysRemaining}d ${hoursRemaining}h`
-      ) : (
-        <span style={{ color: 'red', fontWeight: 'bold', fontSize: '0.75em' }}>
-          EXPIRADO
-        </span>
-      )}
+      {getTimeText(props)}
     </div>
   );
 };
