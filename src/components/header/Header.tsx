529025b66a966e8f1a8645312b5daf51344bab1c
Ivan García García <ivan.garcia@altia.es>
2023-03-23 10:17:03 +0100

    improved header
    now when the counter above the bell is less than 1, it disappears

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,9 @@
 import './header.scss';
 
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { GrNotification } from 'react-icons/gr';
 const maxNoti = 99;
+const minNoti = 1;
 
 interface HeaderProps {
  count: number;
@@ -11,7 +12,17 @@ interface HeaderProps {
 
 export const Header = (props: HeaderProps) => {
   let { count, title } = props;
+  const elementoContador = useRef<HTMLSpanElement>(null); 
 
+  useEffect(() => {
+    if (elementoContador.current) {
+      if (count < minNoti) {
+        elementoContador.current.style.display = 'none';
+      } else {
+        elementoContador.current.style.display = 'flex';
+      }
+    }
+  }, [count]);
 
   if (count >= maxNoti) {
     count = maxNoti;
@@ -22,7 +33,9 @@ export const Header = (props: HeaderProps) => {
       <h1>{title}</h1>
       <div className="notifications">
         <GrNotification />
-        <span className="count">{count}</span>
+        <span className="count" ref={elementoContador}>
+          {count}
+        </span>
       </div>
     </header>
   );
