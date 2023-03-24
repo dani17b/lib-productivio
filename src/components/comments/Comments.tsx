510b5665826373fb67fdf436d4636bcfb5e842a4
FERNANDO VALERIO VEIGA <fernando.valerio@altia.es>
2023-03-24 09:51:59 +0100

    Users added in array

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import { FaComments, FaChevronDown } from 'react-icons/fa';
 import './comments.scss';
 
-
 interface CommentsProps {
-  comments: string[];
+    /**
+   * Array of comments with their corresponding user
+   */
+  comments: { user: string; comment: string }[];
 }
 
 export const Comments = ({ comments }: CommentsProps) => {
@@ -12,14 +14,22 @@ export const Comments = ({ comments }: CommentsProps) => {
 
   return (
     <div className="comments-container">
-      <div className="comment-toggle-container" onClick={() => setShowComments(!showComments)}>
-        <span className="comment-icon">{showComments ? <FaChevronDown /> : <FaComments />}</span>
-        <span className="comment-text">Show Comments</span>
+      <div
+        className="comment-toggle-container"
+        onClick={() => setShowComments(!showComments)}
+      >
+        <span className="comment-icon">
+          {showComments ? <FaChevronDown /> : <FaComments />}
+        </span>
+        <span className="comment-button">Show Comments</span>
       </div>
       {showComments && (
         <div>
-          {comments.map(comment => (
-            <p key={comment}>{comment}</p>
+          {comments.map((comment) => (
+            <div className="comment">
+              <span className="comment-user">{comment.user}</span>
+              <span className="comment-text">{comment.comment}</span>
+            </div>
           ))}
         </div>
       )}
