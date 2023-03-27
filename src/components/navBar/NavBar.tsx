5a885377fc0aa9d7abfc05853c4b91257c7d707c
Ivan García García <ivan.garcia@altia.es>
2023-03-27 09:57:18 +0200

    added onClick

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -2,34 +2,46 @@ import './navBar.scss';
 import { IconType } from 'react-icons/lib/esm/iconBase';
 import { FaHome, FaUserFriends, FaUserAlt } from 'react-icons/fa';
 import { IoMdAdd, IoMdPodium } from 'react-icons/io';
+import React from 'react';
+
 
 interface NavBarProps {
   parentBackgroundColor?: string;
   childBackgroundColor?: string;
   icons: IconType[];
+  onClick: React.MouseEventHandler<HTMLDivElement>;
 }
 
-export const icons: IconType[] = [ FaHome, FaUserFriends, IoMdAdd, IoMdPodium, FaUserAlt ];
+export const icons: IconType[] = [
+  FaHome,
+  FaUserFriends,
+  IoMdAdd,
+  IoMdPodium,
+  FaUserAlt,
+];
 
-
-export const NavBar = ({ parentBackgroundColor, childBackgroundColor, icons }: NavBarProps) => {
-
+export const NavBar = ({
+  parentBackgroundColor,
+  childBackgroundColor,
+  icons,
+  onClick,
+}: NavBarProps) => {
+  
   const parentStyle = {
     backgroundColor: childBackgroundColor,
-    color: parentBackgroundColor,  
+    color: parentBackgroundColor,
   };
 
   const childStyle = {
     backgroundColor: parentBackgroundColor,
-    color: childBackgroundColor,  
+    color: childBackgroundColor,
   };
-  
 
   return (
-    <div className='nav-menu' style={parentStyle}>
-      <nav className='nav-containers' style={childStyle}>
+    <div className="nav-menu" style={parentStyle}>
+      <nav className="nav-containers" style={childStyle}>
         {icons.map((Icon, index) => (
-          <div key={index} className='nav-button'>
+          <div key={index} className="nav-button" onClick={onClick}>
             <Icon />
           </div>
         ))}
