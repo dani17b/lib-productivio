420a44bef81b7dbacd1d749e2cde66b476e70bbd
FerV <116814534+FerV02@users.noreply.github.com>
2023-03-30 12:17:28 +0200

    Cambios hechos

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -4,38 +4,28 @@ import { PostComment } from '../postComment/PostComment';
 import { VisualComments } from '../visualComments/VisualComments';
 import './comments.scss';
 
-const commentProps = {
-  name: 'comment',
-  imageSrc:
-    'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-  inputText: 'Escribe tu comentario aquí...',
-};
+interface CommentProps {
+  name: string;
+  imageSrc: string;
+  inputText: string;
+}
 
-const visualCommentsProps = [
-  {
-    imageSrc:
-      'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-    photoBorderColor: 'blue',
-    username: 'John Doe',
-    comment: 'This is a great post!',
-  },
-  {
-    imageSrc:
-      'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-    photoBorderColor: 'red',
-    username: 'Jane Smith',
-    comment: 'Fantastic content, I loved it!',
-  },
-  {
-    imageSrc:
-      'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-    photoBorderColor: 'green',
-    username: 'Bob Johnson',
-    comment: 'This is very useful, thanks for sharing!',
-  },
-];
+interface VisualCommentsProps {
+  imageSrc: string;
+  photoBorderColor: string;
+  username: string;
+  comment: string;
+}
 
-export const Comments = () => {
+export const Comments = ({
+  commentProps,
+  visualCommentsProps,
+  onPostClick,
+}: {
+  commentProps: CommentProps;
+  visualCommentsProps: VisualCommentsProps[];
+  onPostClick: () => void;
+}) => {
   const [showComments, setShowComments] = useState(false);
 
   return (
@@ -53,7 +43,7 @@ export const Comments = () => {
         <div>
           <div className="Comments">
             <span className="postComment">
-              <PostComment commentProps={commentProps} onClick={() => {}} />
+              <PostComment commentProps={commentProps} onClick={onPostClick} />
             </span>
             {visualCommentsProps.map((visualCommentProps, index) => (
               <span className="visualComment" key={index}>
