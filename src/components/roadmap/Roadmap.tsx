f8a5c2d227432362b6637be98e4662b065739ce3
Paula Seoane <paula.seoane@altia.es>
2023-03-30 12:16:40 +0200

    #36 More documentation

--- src/components/roadmap/Roadmap.tsx
+++ src/components/roadmap/Roadmap.tsx
@@ -1,26 +1,61 @@
 import './roadmap.scss';
 
 enum TaskStatusEnum {
+  /**
+   * Task in progress
+   */
   EN_CURSO = 'En Curso',
+  /**
+   * Task is finished
+   */
   FINALIZADA = 'Finalizada',
 }
 
 interface RoadmapStyle {
+  /**
+   * Changes the circle's size depending on the event displayed
+   */
   circleSize: number;
+  /**
+   * Changes the circle's color depending on the event
+   */
   circleColor: string;
+  /**
+   * Allows you to change the line's color
+   */
   lineColor: string;
+  /**
+   * Allows you to change the line's height so it reaches all the circles displayed
+   */
   lineHeight: number;
 }
 
 interface RoadmapProps {
+  /**
+   * Date when the task is updated or a new level is reached
+   */
   taskDate: Date;
+  /**
+   * Task name or description
+   */
   taskName?: string;
+  /**
+   * Displays the new level once it's reached
+   */
   level?: number;
+  /**
+   * Allows to change the task status from "EN_CURSO" to "FINALIZADA"
+   */
   status?: TaskStatusEnum;
+  /**
+   * Various style options that change depending on the information displayed
+   */
   style: RoadmapStyle;
 }
 
 export const Roadmap = (props: RoadmapProps) => {
+  const { taskDate, taskName, level, status, style } = props;
+
   return (
     <div className="roadmap-container">
       <div className="roadmap-container__left-column"></div>
