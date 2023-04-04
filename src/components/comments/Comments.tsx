9908e33e92728037ac68b12999308f52f84fb394
FerV <116814534+FerV02@users.noreply.github.com>
2023-04-04 16:40:43 +0200

    CAmbio de estilos

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,58 +1,18 @@
-import React, { useState } from 'react';
-import { FaComments, FaChevronDown } from 'react-icons/fa';
-import { PostComment } from '../postComment/PostComment';
-import { VisualComments } from '../visualComments/VisualComments';
+import { FaComments } from 'react-icons/fa';
 import './comments.scss';
 
-interface CommentProps {
-  name: string;
-  imageSrc: string;
-  inputText: string;
-}
-
-interface VisualCommentsProps {
-  imageSrc: string;
-  photoBorderColor: string;
-  username: string;
-  comment: string;
-}
-
 export const Comments = ({
-  commentProps,
-  visualCommentsProps,
-  onPostClick,
+  onShowCommentsClick,
 }: {
-  commentProps: CommentProps;
-  visualCommentsProps: VisualCommentsProps[];
-  onPostClick: () => void;
+  onShowCommentsClick: () => void;
 }) => {
-  const [showComments, setShowComments] = useState(false);
-
   return (
     <div className="comments-container">
-      <div
-        className="comment-toggle-container"
-        onClick={() => setShowComments(!showComments)}
-      >
-        <span className="comment-icon">
-          {showComments ? <FaChevronDown /> : <FaComments />}
-        </span>
-        <span className="comment-button">Show Comments</span>
+      <div className="comment-toggle-container">
+        <button className="comment-button" onClick={onShowCommentsClick}>
+          <FaComments />
+        </button>
       </div>
-      {showComments && (
-        <div>
-          <div className="Comments">
-            <span className="postComment">
-              <PostComment commentProps={commentProps} onClick={onPostClick} />
-            </span>
-            {visualCommentsProps.map((visualCommentProps, index) => (
-              <span className="visualComment" key={index}>
-                <VisualComments commentProps={visualCommentProps} />
-              </span>
-            ))}
-          </div>
-        </div>
-      )}
     </div>
   );
 };
